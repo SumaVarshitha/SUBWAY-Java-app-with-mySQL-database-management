427f9a6a96d65f80514bdf23fcbce7efdf5fe879
Kartik Nighania <kkstrack@gmail.com>
2016-07-14 11:36:43 +0530

    added java code

---
+++ hotel management - Java Part/KARTIK & PIYUSH IP PROJECT/src/kartiks/ip/project/KARTIKSIPPROJECT.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kartiks.ip.project;
+
+/**
+ *
+ * @author KARTIK NIGHANIA
+ */
+public class KARTIKSIPPROJECT {
+
+  
+    public static void main(String[] args) {
+       java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LOGIN_FORM().setVisible(true);
+            }
+        });
+    
+        
+        
+    }
+}
